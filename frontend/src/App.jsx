ad4430075be3ddd0935292c7962e370ae40d90ff
Bryan <BryanCrombach@gmail.com>
2025-06-30 17:56:43 +1200

    feat(frontend): configure tailwincss, daisyui, and react-hot-toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,17 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
 import NoteDetailPage from "./pages/NoteDetailPage.jsx";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
+    <div data-theme="forest">
+      <button
+        className="btn btn-primary"
+        onClick={() => toast.success("Thank You")}
+      >
+        CLICK ME!!!
+      </button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const CreatePage = () => {
   return <div>CreatePage</div>;
 };
