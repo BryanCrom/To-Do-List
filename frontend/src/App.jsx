cb3f43a0115d42b4d0956e4b38d74e11d25ad56d
Bryan <BryanCrombach@gmail.com>
2025-07-01 18:56:12 +1200

    feat(frontend): create Navbar component and add versions to dependency installs project wide

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,10 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
 import NoteDetailPage from "./pages/NoteDetailPage.jsx";
-import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div data-theme="forest">
-      <button
-        className="btn btn-primary"
-        onClick={() => toast.success("Thank You")}
-      >
-        CLICK ME!!!
-      </button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
