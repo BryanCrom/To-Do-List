58bd9392feb7767c5fc5a08a8b1c9568b343b5bb
Bryan <BryanCrombach@gmail.com>
2025-07-08 18:09:47 +1200

    feat(deploy): add all necessary changes for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import path from "path";
+
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
 //import requestTracker from "./middleware/requestTracker.js";
@@ -10,19 +12,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Backend started on PORT: " + PORT);
-  });
-});
-
-//apply cors
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  //apply cors
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 //lets us use req.body
 app.use(express.json());
@@ -34,3 +33,17 @@ app.use(rateLimiter);
 //app.use(requestTracker);
 
 app.use("/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Backend started on PORT: " + PORT);
+  });
+});
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5123" });
+//localhost doesn't exist in production so the base url must be dynamic
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5123" : "";
+
+const api = axios.create({ baseURL: BASE_URL });
 
 export default api;
